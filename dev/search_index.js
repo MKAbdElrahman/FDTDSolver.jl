6d5941452587e79c39f139a6eace2f62ba42ca34
Documenter.jl <documenter@juliadocs.github.io>
2022-02-23 07:53:50 +0000

    build based on 55ca7cd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FDTDSolver","category":"page"},{"location":"#FDTDSolver","page":"Home","title":"FDTDSolver","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FDTDSolver.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FDTDSolver]","category":"page"}]
+}
