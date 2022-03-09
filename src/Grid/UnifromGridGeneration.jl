0379c09d3c488c38457a96bbd446cf910e41f14c
MKAbdElrahman <smohamed.abdelrahman@gmail.com>
2022-03-09 04:56:48 +0200

    Added a method to find optimal spacing for a uniform  structured grid

--- src/Grid/UnifromGridGeneration.jl
+++ src/Grid/UnifromGridGeneration.jl
@@ -1,8 +1,8 @@
 """
 function optimal_cell_size(Δ_min, Δ_max, x_c; n_particles = 100, p_norm = Inf)
 
-Uses particle swarm optimization to find a cell size in the range
-[Δ_min,Δ_max] such that the nodes `x_c` are integer multiples of this cell size.
+Uses particle swarm optimization to return a cell size in the range
+[Δ_min,Δ_max] such that the nodes `x_c` are integer multiples of the found cell size.
 
 
 ...
@@ -12,7 +12,9 @@ Uses particle swarm optimization to find a cell size in the range
 - `x_c`: constraint node coordinates
 - `n_particles`: number of particles in the swarm (default: 100)
 - `p_norm`: norm used to calculate the misfit objective function (default: Inf)
+- `range`: set true to return a `LinRange` instead  of cell spacing.
 ...
+
 
 # Examples
 ```julia-repl
@@ -37,7 +39,6 @@ julia> optimal_cell_size(Δ_min,Δ_max,x_c)
 The details of the algorithm are described in:
 "Structured Mesh Generation: Open-source automatic nonuniform mesh generation for FDTD simulation."
 link to the paper: https://ieeexplore.ieee.org/document/7458133
-...
 """
 function optimal_cell_size(Δ_min, Δ_max, x_c; n_particles = 100, p_norm = Inf,range = false)
     x0 = min(x_c...)
@@ -50,6 +51,6 @@ function optimal_cell_size(Δ_min, Δ_max, x_c; n_particles = 100, p_norm = Inf,
     objective(M) = norm(abs.(M[1] / Lx * x_c - round.(M[1] / Lx * x_c)), p_norm)
     @info "finding optimal spacing..."
     op = optimize(objective, [M_initial], ParticleSwarm(; lower = [M_min], upper = [M_max], n_particles = n_particles))
-    @info "optimal step found $(Lx/round.(Int,op.minimizer[1])) with loss = $(op.minimum)"
+    @info "optimal step found is $(Lx/round.(Int,op.minimizer[1])) with loss = $(op.minimum)"
     return range ? LinRange(x0,xL,round.(Int, op.minimizer[1] + 1))  :  Lx / round.(Int, op.minimizer[1])
 end
